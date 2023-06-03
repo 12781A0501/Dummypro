1c4532e27b033f303447d8c9f35be7da590b3836
tharun <tharun@LAPTOP-F1JLMS6E>
2023-06-03 18:18:32 +0530

    First Commit

---
+++ src/main/java/dummy/Testcase1.java
@@ -0,0 +1,16 @@
+package dummy;
+
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	@Test
+	public void m1()
+	{
+	Reporter.log("m1 is excetuded");
+	}
+
+}
+
---
+++ src/main/java/dummy/Testcase2.java
@@ -0,0 +1,15 @@
+package dummy;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+	
+		@Test
+		public void m2()
+		{
+		Reporter.log("m2 is excetuded");
+		}
+
+	}
